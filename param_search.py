ee9a963b875489d1083a893d6e507333810c1a0c
Ysmiraak <ysmiraak@gmail.com>
2017-03-24 08:32:31 +0800

    cleanup

--- param_search.py
+++ param_search.py
@@ -5,22 +5,23 @@ ud_path = "/data/ud-treebanks-conll2017/"
 wv_path = "/data/udpipe-ud-2.0-conll17-170315-supplementary-data/" \
           "ud-2.0-baselinemodel-train-embeddings/"
 
-setup = Setup.build(
-    ud_path + "UD_Ancient_Greek-PROIEL/grc_proiel-ud-train.conllu",
-    wv_path + "grc_proiel.skip.forms.50.vectors",
-    labeled=True)
-setup.save("./setups/grc_proiel-labeled.npy")
-del setup
-setup = Setup.build(
-    ud_path + "UD_Ancient_Greek-PROIEL/grc_proiel-ud-train.conllu",
-    wv_path + "grc_proiel.skip.forms.50.vectors",
-    labeled=False)
-setup.save("./setups/grc_proiel-unlabeled.npy")
+# setup = Setup.build(
+#     ud_path + "UD_Ancient_Greek-PROIEL/grc_proiel-ud-train.conllu",
+#     wv_path + "grc_proiel.skip.forms.50.vectors",
+#     labeled=True)
+# setup.save("./setups/grc_proiel-labeled.npy")
+# del setup
+# setup = Setup.build(
+#     ud_path + "UD_Ancient_Greek-PROIEL/grc_proiel-ud-train.conllu",
+#     wv_path + "grc_proiel.skip.forms.50.vectors",
+#     labeled=False)
+# setup.save("./setups/grc_proiel-unlabeled.npy")
 
-# setup = Setup.load("./setups/grc_proiel-labeled.npy")
 # setup = Setup.load("./setups/grc_proiel-unlabeled.npy")
 
-# dev = list(load(ud_path + "/UD_Ancient_Greek-PROIEL/grc_proiel-ud-dev.conllu"))
+setup = Setup.load("./setups/grc_proiel-labeled.npy")
+
+dev = list(load(ud_path + "/UD_Ancient_Greek-PROIEL/grc_proiel-ud-dev.conllu"))
 
 # # search for optimizer
 # for optimizer in ('sgd', 'rmsprop', 'adagrad', 'adadelta',
