593dac5edf499f72c6a39457f986ee0425c1b08c
Ysmiraak <ysmiraak@gmail.com>
2017-03-29 17:48:51 +0800

    new setup

--- x_setup.py
+++ x_setup.py
@@ -2,24 +2,41 @@ from nn_mlp import Setup
 from conllu import load, write
 
 ud_path = "/data/ud-treebanks-conll2017/"
-wv_path = "/data/udpipe-ud-2.0-conll17-170315-supplementary-data/" \
-          "ud-2.0-baselinemodel-train-embeddings/"
 
-write(load(ud_path + "UD_Ancient_Greek-PROIEL/grc_proiel-ud-dev.conllu"),
-      "./setups/grc_proiel-ud-dev.conllu")
+
+# ancient greek proiel
+lang = "grc_proiel"
+dev_path = ud_path + "UD_Ancient_Greek-PROIEL/grc_proiel-ud-dev.conllu"
+train_path = ud_path + "UD_Ancient_Greek-PROIEL/grc_proiel-ud-train.conllu"
+
+# # chinese
+# lang = "zh"
+# dev_path = ud_path + "UD_Chinese/zh-ud-dev.conllu"
+# train_path = ud_path + "UD_Chinese/zh-ud-train.conllu"
+
+
+write(load(dev_path), "./setups/{}-ud-dev.conllu".format(lang))
+
+embedding_path = "/data/udpipe-ud-2.0-conll17-170315-supplementary-data/" \
+                 "ud-2.0-baselinemodel-train-embeddings/" \
+                 "{}.skip.forms.50.vectors".format(lang)
 
 setup = Setup.build(
-    ud_path + "UD_Ancient_Greek-PROIEL/grc_proiel-ud-train.conllu",
-    wv_path + "grc_proiel.skip.forms.50.vectors",
-    labeled=True)
+    train_path,
+    embedding_path,
+    labeled=True,
+    projective=True
+)
 
-setup.save("./setups/grc_proiel-labeled.npy")
+setup.save("./setups/{}-labeled.npy".format(lang))
 
-# del setup
+del setup
 
-# setup = Setup.build(
-#     ud_path + "UD_Ancient_Greek-PROIEL/grc_proiel-ud-train.conllu",
-#     wv_path + "grc_proiel.skip.forms.50.vectors",
-#     labeled=False)
+setup = Setup.build(
+    train_path,
+    embedding_path,
+    labeled=False,
+    projective=True
+)
 
-# setup.save("./setups/grc_proiel-unlabeled.npy")
+setup.save("./setups/{}-unlabeled.npy".format(lang))
