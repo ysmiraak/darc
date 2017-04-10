8fe3cd8f5190e06461dfbb2c380c97ae0b9fafd7
ysmiraak <ysmiraak@gmail.com>
2017-04-10 15:41:33 +0200

    minor

--- x2_setup.py
+++ x2_setup.py
@@ -8,7 +8,7 @@ def setup(lang,
           proj=False,
           embed_path="./embed/",
           setup_path="./setups/"):
-    save(load(path(lang, 'dev')), "./golds/{}-ud-dev.conllu".format(lang))
+    # save(load(path(lang, 'dev')), "./golds/{}-ud-dev.conllu".format(lang))
     Setup \
         .make(path(lang, 'train'),
               form_w2v="{}{}{}.form".format(embed_path, lang, suffix),
@@ -19,9 +19,14 @@ def setup(lang,
 
 
 if '__main__' == __name__:
-    from sys import argv
-    try:
-        argv[3] = bool(argv[3])
-    except IndexError:
-        pass
-    setup(*argv[1:])
+    # from sys import argv
+    # try:
+    #     argv[3] = bool(argv[3])
+    # except IndexError:
+    #     pass
+    # setup(*argv[1:])
+    lang = 'la_proiel'
+    for w in 4, 5, 6, 7, 8:
+        suffix = "-w{}s1n8i16".format(w)
+        print(lang, suffix)
+        setup(lang, suffix)
--- x3_train.py
+++ x3_train.py
@@ -2,19 +2,26 @@ from ud2 import path
 from setup import Setup
 from conllu import load, save
 
+
+def ready(lang, suffix):
+    setup = Setup.load("./setups/{}{}.npy".format(lang, suffix))
+    dev = list(load(path(lang, 'dev')))
+    def train(infix="", epochs=10, **kwargs):
+        model = setup.model(**kwargs)
+        for epoch in range(epochs):
+            setup.train(model, verbose=2)
+            save((setup.parse(model, sent) for sent in dev),
+                 "./results/{}{}-e{:0>2d}.conllu"
+                 .format(lang, infix, epoch))
+    return train
+
+
 lang, suffix = 'la_proiel', '-nonp'
 # lang, suffix = 'fa', 'nonp'
 # lang, suffix = 'grc_proiel', 'nonp'
 # lang, suffix = 'zh', 'proj'
 
-setup = Setup.load("./setups/{}{}.npy".format(lang, suffix))
-dev = list(load(path(lang, 'dev')))
-
-
-def train(infix="", epochs=10, **kwargs):
-    model = setup.model(**kwargs)
-    for epoch in range(epochs):
-        setup.train(model, verbose=2)
-        save((setup.parse(model, sent) for sent in dev),
-             "./results/{}{}-e{:0>2d}.conllu"
-             .format(lang, infix, epoch))
+infixes = ["-w{}s1n8i16".format(w) for w in 4, 5, 6, 7, 8]
+infix = infixes[0]
+train = ready(lang, suffix + infix)
+train(infix, 16)
