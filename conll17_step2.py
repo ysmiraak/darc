2c2224c51cd951ae59af599522f48f7e38d78894
ysmiraak <ysmiraak@gmail.com>
2017-04-18 00:23:06 +0200

    consider start running

--- conll17_step2.py
+++ conll17_step2.py
@@ -1,35 +1,33 @@
-import src_ud2 as ud2
-import src_conllu as conllu
 from src_setup import Setup
-import json
+import src_ud2 as ud2
+import numpy as np
 
 silver_train_path = "./lab/silver_train/"
 pretrain_w2v_path = "./lab/pretrain_w2v/"
 system_model_path = "./lab/system_model/"
 
 
-lang = "kk"
-
-def make_setup(lang):
+def make_setup(lang, proj):
+    """-> Setup"""
     silver = "{}{}.conllu".format(silver_train_path, lang)
     form_w2v = "{}{}-form.w2v".format(pretrain_w2v_path, lang)
     lemm_w2v = "{}{}-lemm.w2v".format(pretrain_w2v_path, lang) \
                if lang not in ud2.no_lemma else None
-    return Setup.make(silver, form_w2v, lemm_w2v, proj=False)
+    return Setup.make(silver, form_w2v, lemm_w2v, proj=proj)
 
 
-setup = make_setup(lang)
-model = setup.model()
-setup.train(model, epochs=8)
+def train_save(setup, suffix):
+    """save the setup and model weights from epoch 4 to 16"""
+    setup.save("{}{}{}.npy".format(system_model_path, lang, suffix), with_data=False)
+    model = setup.model()
+    for epoch in range(16):
+        setup.train(model, verbose=2)
+        if 4 <= epoch:
+            np.save("{}{}{}-e{:0>2d}.npy".format(system_model_path, lang, suffix, epoch),
+                    {'model': model.to_json(), 'weights': model.get_weights()})
 
-dev = ud2.path(lang, 'train')
 
-conllu.save((setup.parse(model, sent) for sent in conllu.load(dev)),
-            "./lab/{}-system.conllu".format(lang))
-
-setup.save("{}{}.npy".format(system_model_path, lang), model, with_data=False)
-
-setup, model = Setup.load("{}{}.npy".format(system_model_path, lang), with_model=True)
-
-conllu.save((setup.parse(model, sent) for sent in conllu.load(dev)),
-            "./lab/{}-system2.conllu".format(lang))
+if '__main__' == __name__:
+    lang = argv[1]
+    train_save(make_setup(lang, False), "-nonp")
+    train_save(make_setup(lang, True), "-proj")
