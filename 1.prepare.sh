e8f1489aec2527f28248c5f7e2aa724c79366906
ysmiraak <ysmiraak@gmail.com>
2017-04-25 22:38:10 +0200

    revert

--- 1.prepare.sh
+++ 1.prepare.sh
@@ -5,7 +5,7 @@
 #  hu id it it_partut ja kk ko la la_ittb la_proiel lv nl nl_lassysmall no_bokmaal no_nynorsk pl
 #  pt pt_br ro ru ru_syntagrus sk sl sl_sst sv sv_lines tr ug uk ur vi zh"
 
-ud2_treebank_path="/data/ud-2.0-conll17-baselinemodel-split/"
+ud2_treebank_path="/data/ud-treebanks-conll2017/"
 udpipe_model_path="./conll17/udpipe_model/"
 silver_train_path="./conll17/silver_train/"
 udpiped_test_path="./conll17/udpiped_test/"
@@ -18,13 +18,13 @@ for lang in ${ud2}
 do
     model=${udpipe_model_path}${lang}".udpipe"
     # preparing training data
-    gold=$(find ${ud2_treebank_path}*"/"${lang}"-ud-baselinemodel-train.conllu")
-    tune=$(find ${ud2_treebank_path}*"/"${lang}"-ud-baselinemodel-tune.conllu")
+    gold=$(find ${ud2_treebank_path}*"/"${lang}"-ud-train.conllu")
     silver=${silver_train_path}${lang}".conllu"
-    udpipe --input conllu --tag --outfile ${silver} ${model} ${gold} ${tune}
+    udpipe --input conllu --tag --outfile ${silver} ${model} ${gold}
     echo "written" ${silver}
     # preparing testing data
-    raw=$(find ${ud2_treebank_path}*"/"${lang}"-ud-baselinemodel-dev.txt")
+    raw=$(find ${ud2_treebank_path}*"/"${lang}"-ud-dev.txt" ||
+              find ${ud2_treebank_path}*"/"${lang}"-ud-train.txt")
     piped=${udpiped_test_path}${lang}".conllu"
     udpipe --input horizontal --tokenize --tag --outfile ${piped} ${model} ${raw}
     echo "written" ${piped}
